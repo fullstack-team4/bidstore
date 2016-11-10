b72572ffbb77ca987a107dc8c461314b433311ba
liuyouzhen <liuyouzhen@gmail.com>
2016-11-10 21:03:19 +0800

    add products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,20 @@
+class ProductsController < ApplicationController
+
+    def index
+      @products = Product.all
+    end
+
+
+    def show
+      @product = Product.find(params[:id])
+    end
+
+
+
+    private
+
+    def product_params
+      params.require(:product).permit(:title, :description, :product, :quantity, :price)
+    end
+
+end
