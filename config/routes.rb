c5b35b8e3dc59a92a44179141b8febea43dd64a6
feiben <feiben365@gmail.com>
2016-11-10 22:50:54 +0800

    Step 1 : 建立结帐页 routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :carts
+  resources :carts do
+    collection do
+      post :checkout
+    end
+  end
 
   resources :products do
     member do
