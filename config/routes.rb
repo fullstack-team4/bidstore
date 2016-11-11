3f086dfed0ff3d2a33f2d3784bfc958f73493e1a
chiyaki <66chiyaki@gmail.com>
2016-11-11 14:02:15 +0800

    generate controller account/orders and admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,15 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
+    resources :orders
+  end
+
+  namespace :account do
+    resources :orders
   end
 
   resources :orders
-  
+
   root 'products#index'
 
 end
