ad3b66815414f5f69525818d7192a6bf4859197e
William <william@WilliamtekiMacBook-Pro.local>
2016-11-11 11:23:36 +0800

    implement image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110115224) do
+ActiveRecord::Schema.define(version: 20161111030619) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20161110115224) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
