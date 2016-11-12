e1ee164ac1eb1061fb93483dc7d56646401553e8
chiyaki <66chiyaki@gmail.com>
2016-11-12 14:41:07 +0800

    用户能够清空购物车，将商品从购物车移除，申请取消订单

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,14 @@
 class CartsController < ApplicationController
+  def clean
+    current_cart.clean!
+    flash[:warning] = "已清空购物车"
+    redirect_to carts_path
+  end
+
+  def clean!
+    cart_items.destroy_all
+  end
+
   def checkout
     @order = Order.new
   end
