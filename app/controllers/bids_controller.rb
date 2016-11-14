60530959db77f40254ae8b6dc8da404c389f491e
William <william@WilliamtekiMacBook-Pro.local>
2016-11-14 20:17:16 +0800

    add bid function

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -2,26 +2,69 @@ class BidsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @bids = Bids.order("created_at DESC")
+    @bids = Bid.all
   end
 
   def create
     @product = Product.find(params[:product_id])
     @bid = Bid.new(bid_params)
-    @bid.user = current_user
-    @bid.product = @product
 
     if @bid.save
-      redirect_to @bid.product, flash: {success: "Bidded"}
+      flash[:notice] = "竞价成功！！！."
+      redirect_to new_product_bid_path
     else
-      redirect_to @bid.product, flash: {warning: "bid must be higher than current bid"}
+      flash[:warning] = "竞价失败.请重新出价！！"
+      redirect_to new_product_bid_path
     end
+
+    # if @product.bids.present?
+    #   if @bid.amount > @product.bids.last.amount
+    #     @bid.save
+    #     flash[:notice] = "Bid has been successfully placed."
+    #     redirect_to product_bids_path
+    #   else
+    #     flash[:warning] = "竞价失败.请重新出价！！"
+    #     redirect_to new_product_bid_path
+    #   end
+    # else
+    #   if @bid.amount > @product.price
+    #     @bid.save
+    #     flash[:notice] = "Bid has been successfully placed."
+    #     redirect_to product_bids_path
+    #   else
+    #     flash[:warning] = "竞价失败.请重新出价！！"
+    #     redirect_to new_product_bid_path
+    #   end
+    # end
+
+    # if @bid.amount > @product.bi
+    #   flash[:notice] = "Bid has been successfully placed."
+    #   redirect_to product_bids_path
+    # else
+    #   flash[:warning] = "竞价失败.请重新出价！！"
+    #   redirect_to new_product_bid_path
+    # end
+  end
+
+
+  def show
+    @product = Product.find(params[:product_id])
+    @bid = Bid.new(params[:id])
+    @bid.user = current_user
+    @bid.product = @product
+  end
+
+  def new
+    @product = Product.find(params[:product_id])
+    @bid = Bid.new
   end
 
   private
 
   def bid_params
-    params.require(:bid)permit(:amount, :product_id)  
+    params.require(:bid).permit(:amount, :product_id, :user_id)
   end
+
+
 
 end
--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,16 +1,61 @@
 class Bid < ApplicationRecord
   belongs_to :product
   belongs_to :user
-  before_create :check_price_is_greater_than_current_bid
+  before_validation :check_price_is_greater_than_current_bid
   validates :amount, numericality: {greater_than_or_equal_to: 1}
 
-  def check_price_is_greater_than_current_bid
-    binding.pry
-    unless  product.bids.present? && amount > product.bids.last.amount
-      errors.add(:amount, "Bid must be greater than current bid")
-      false
-    end
-  end
 
+  # def check_price_is_greater_than_current_bid
+  #   if product.bid.amount > product.price
+  #     @bid.save
+  #   else
+  #     redirect_to new_product_bid_path
+  #     flash[:warning] = "出价过低，请重新出价！"
+  #   end
+  # end
+
+  def check_price_is_greater_than_current_bid
+
+    if product.bids.present?
+      if amount > product.bids.last.amount
+        return true
+      else
+        return false
+      end
+    else
+      if amount > product.price
+        return true
+      else
+        return false
+      end
+    end
+
+    # if product.bids.present?
+    #   if amount > product.bids.last.amount
+    #     return true
+    #   else
+    #     return false
+    #   end
+    # else
+    #   if amount > product.price
+    #     return true
+    #   else
+    #     return false
+    #   end
+    # end
+    # puts "!!!!!!!!!!!!!!!!amount:#{amount}"
+    # puts "!!!!!!!!!!!!!!!!product.bids.last.amount:#{product.bids.last.amount}"
+    # puts "#{amount > product.bids.last.amount}"
+    #
+    # if product.bids.present? && amount > product.bids.last.amount
+    #
+    #   puts "!!!!!!!!!!!!!!!!!!!!!!!!valid"
+    #   return true
+    # else
+    #   errors.add(:amount, "Bid must be greater than current bid")
+    #   puts "!!!!!!!!!!!!!!!!!!!!!!!!invalid"
+    #   return false
+    # end
+  end
 
 end
