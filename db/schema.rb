69cf33a7e5bd673316581395d7d12e32fcfd5382
chiyaki <66chiyaki@gmail.com>
2016-11-16 21:42:44 +0800

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116095459) do
+ActiveRecord::Schema.define(version: 20161116122913) do
 
   create_table "bids", force: :cascade do |t|
     t.integer  "product_id"
@@ -56,10 +56,9 @@ ActiveRecord::Schema.define(version: 20161116095459) do
     t.string   "product_name"
     t.integer  "product_price"
     t.integer  "quantity"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "product_id"
-    t.integer  "product_buyout"
   end
 
   create_table "product_relationships", force: :cascade do |t|
