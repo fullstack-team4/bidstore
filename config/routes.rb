83f31028e200005fc305b5882e8b5e2aa9eae962
william0017 <wangwei0017@gmail.com>
2016-11-22 01:44:26 +0800

    amount-order-done

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,16 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :bid do
+    resources :orders do
+      member do
+        post :pay_with_alipay
+        post :pay_with_wechat
+        post :apply_to_cancel
+      end
+    end
+  end 
+
 
 
 
