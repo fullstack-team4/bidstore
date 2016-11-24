732bd452229798b9c46dac5e7389139926b06747
feiben <feiben365@gmail.com>
2016-11-24 08:51:40 +0800

    12 seeds with 3 tags created

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,9 +63,10 @@ create_product = Product.create([title: "李笑来的法拉利",
                                  buyout:8000000,
                                  image: "http://m4.auto.itc.cn/c_zoom,w_800/800379.jpg",
                                  is_hidden:"false",
+                                 tag:"网红",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:09:00",
-                                 endtime: "2016-11-16 09:09:00"])
+                                 endtime: "2016-11-16 09:09:00" ])
 puts "Product created"
 
 create_product = Product.create([title: "罗永浩的锤子",
@@ -75,6 +76,7 @@ create_product = Product.create([title: "罗永浩的锤子",
                                  buyout: 6666,
                                  image: "http://image.nbd.com.cn/uploads/articles/thumbnails/1074/4.x_large.jpg",
                                  is_hidden:"false",
+                                 tag:"",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-17 18:09:00",
                                  endtime: "2016-11-18 09:09:00"])
@@ -87,6 +89,7 @@ create_product = Product.create([title: "韩寒的摄像机",
                                  buyout: 99999,
                                  image: "http://s.qdcdn.com/cl/12441845,800,450.jpg",
                                  is_hidden:"false",
+                                 tag:"",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-20 18:09:00",
                                  endtime: "2016-11-22 09:09:00"])
@@ -99,6 +102,163 @@ create_product = Product.create([title: "小S的牛仔裤",
                                  buyout: 8888,
                                  image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
                                  is_hidden:"false",
+                                 tag:"",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "明星A的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"明星",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "明星B的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"明星",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "明星C的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"明星",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "明星D的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"明星",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "健将A的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"健将",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "健将B的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"健将",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "健将C的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"健将",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "健将D的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"健将",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "网红A的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"网红",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "网红B的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"网红",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "网红C的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"网红",
+                                 aasm_state: "before_auction",
+                                 begintime: "2016-11-15 18:09:00",
+                                 endtime: "2016-11-25 09:09:00"])
+puts "Product created"
+
+create_product = Product.create([title: "网红D的宝贝",
+                                 description: "QQQ",
+                                 price: 888,
+                                 quantity: 100,
+                                 buyout: 8888,
+                                 image: "http://ww2.sinaimg.cn/mw690/6592c2e0jw1f85mgtlpqzj20qo0f0tb9.jpg",
+                                 is_hidden:"false",
+                                 tag:"网红",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:09:00",
                                  endtime: "2016-11-25 09:09:00"])
