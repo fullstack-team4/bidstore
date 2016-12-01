155d82acabb445e3490e3ceea5d709dd370f6972
chiyaki <66chiyaki@gmail.com>
2016-12-02 01:55:21 +0800

    fix seed welcome

--- db/seeds.rb
+++ db/seeds.rb
@@ -66,7 +66,8 @@ create_product = Product.create([title: "李笑来的法拉利",
                                  tag:"netceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-16 09:00:00" ])
+                                 endtime: "2016-12-16 09:00:00",
+                                 product_story: "自从李笑来，老师在得到专栏上开设了通往财富自由之路的专栏以后，争议不断，很多人都吐槽，说这其实是李笑来老师自己又是上了财务自由之路。现在，李笑来老师抛出了他心爱的保时捷赛车，给你也提供了一个机会，驾驶着这位财神的座驾，行驶在你自己的通往财富自由之路上。" ])
 puts "Product created"
 
 create_product = Product.create([title: "罗永浩的锤子",
@@ -79,7 +80,8 @@ create_product = Product.create([title: "罗永浩的锤子",
                                  tag:"netceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-17 18:00:00",
-                                 endtime: "2016-12-18 09:00:00"])
+                                 endtime: "2016-12-18 09:00:00",
+                                 product_story: "彪悍的人生不要解释！彪悍的大锤更不需要解释：大锤落下，草木皆毁。 据说此锤的头是坚硬的神铁所打造而成，握柄部分则是以世界之树的木材作成。不用的时候还可以缩小放进口袋里。此锤的功能为可以击穿任何东西，要是把它扔出去，它会毫无误差地击中目标，而且飞出去之后还会自动飞回持有者的手中。"])
 puts "Product created"
 
 create_product = Product.create([title: "韩寒的摄像机",
@@ -92,7 +94,8 @@ create_product = Product.create([title: "韩寒的摄像机",
                                  tag:"netceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-20 18:00:00",
-                                 endtime: "2016-12-22 09:00:00"])
+                                 endtime: "2016-12-22 09:00:00",
+                                 product_story: "这是一个偶像幻灭的时代，这是一个崇拜坍塌的时代。16年前，韩寒凭《杯中窥人》一朝成名，以叛逆少年的形象出现在世人眼中，被贴上“青年领袖、公共知识分子”等各种标签，成为中国80后一代的形象代言人。16年后，不可否认，韩寒已经从一个符号变成一个凡人，在摄像机下重新定位“每一个自己”。"])
 puts "Product created"
 
 create_product = Product.create([title: "小S的牛仔裤",
@@ -105,7 +108,8 @@ create_product = Product.create([title: "小S的牛仔裤",
                                  tag:"movieceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "现如今，它依旧是阔腿裤潮流下的人气单品。霸道女总裁爱它、下身不够瘦的姑娘爱它、怕冻的姑娘更爱它！冬日外套的最强内搭，显瘦又御寒！搭配长大衣，气场翻倍！搭配短夹克，秀出大长腿！人造皮早的温暖质感与心机内搭，零下七度能耐我如何？"])
 puts "Product created"
 
 create_product = Product.create([title: "周星驰的剧本",
@@ -118,7 +122,8 @@ create_product = Product.create([title: "周星驰的剧本",
                                  tag:"movieceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "《大话西游》、《西游降魔》，每个人最打动人心的作品，正是他自己的命运剧本。它们有共同的模式：最爱的女子，男主角一直在躲避，直到她用生死证明爱，男主角才悲痛成神。星爷真诚地从自己的内心出发，都非常用力，都是在表达自己。人活着，就是要跳出自己的命运剧本，能碰触到、理解到并能表达出自己的命运剧本，实属难得。"])
 puts "Product created"
 
 create_product = Product.create([title: "周杰伦钢琴.",
@@ -131,7 +136,8 @@ create_product = Product.create([title: "周杰伦钢琴.",
                                  tag:"movieceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "周董如愿娶回真命天女昆凌，盛大的英国教堂世纪婚礼，如同王子与公主的童话故事。回首周杰伦的感情史，“J女郎”名单一大串，昆凌到底是怎么与周董相识、相恋？比周董小14岁的昆凌，当初是因为在周董投资的潮服店兼职而结识，后来周董介绍她学钢琴，如此诗意的场景下，浪漫情节,周董表白说：“我的字不好看啦!也不会去写什么留下证据，表白可能以弹钢琴比较多，音乐是一种语言！”"])
 puts "Product created"
 
 create_product = Product.create([title: "李宗盛吉他",
@@ -144,7 +150,9 @@ create_product = Product.create([title: "李宗盛吉他",
                                  tag:"movieceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "从音乐人向制琴师跨界，李宗盛大哥翻越了一座人生的“山丘”。十多年来，他为了梦想一路坚持，就像一把原木吉他，任凭时光打磨，始终保持演奏最本色的音符。入行三十年，写了三百首歌，45岁这一年，李宗盛正式决定开始做自己的品牌“李吉他”（Lee Guitar）。他说：“我知道手艺人往往意味着固执、缓慢、少量、劳作。但是，这些背后所隐含的是专注、技艺、对完美的追求。”
+"])
 puts "Product created"
 
 create_product = Product.create([title: "胡歌的相机",
@@ -157,7 +165,8 @@ create_product = Product.create([title: "胡歌的相机",
                                  tag:"movieceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "胡歌，大家熟知的大明星。十六七岁开始学习摄影，就是纯粹想泡妞，感觉背着相机比抱着篮球更有范儿。在他看来：胶片是信仰，数码是工具。对于胡歌来说，摄影有些不一样的意义。相机陪他走过了小半个地球，也伴他度过了人生中的一次次坎坷，“皮囊坏了，就用思想填满它”。"])
 puts "Product created"
 
 create_product = Product.create([title: "李娜的网球拍",
@@ -170,7 +179,8 @@ create_product = Product.create([title: "李娜的网球拍",
                                  tag:"sportceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "真实的活在我们这个世界的，不停战斗的约翰克里斯朵夫。在年轻的时候，她从不妥协，这对曾经除了彼此以外一无所有的夫妻，如何凭借自己的努力，从默默无闻的角落，走到了世界瞩目的中心。说出来会被嘲笑的梦想，才有实现的价值，而她如今做到的，远超过曾经许下的梦想。没有被这个世界，把自己变成曾经讨厌的模样。"])
 puts "Product created"
 
 create_product = Product.create([title: "郎平的排球",
@@ -183,7 +193,8 @@ create_product = Product.create([title: "郎平的排球",
                                  tag:"sportceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "郎平，从运动员时代开始，就与各项璀璨荣誉并肩而行，执教期间她更是点石成金，让她从完美升华为伟大。所谓王者，必然要超脱时空的局限。从上世纪80年代的“铁榔头”，到今日的“女排教主”，从中国到意大利到美国再回到中国，郎平用30年光阴岁月，证明了自己，是当之无愧的中国三大球第一人。"])
 puts "Product created"
 
 create_product = Product.create([title: "姚明的篮球",
@@ -196,7 +207,8 @@ create_product = Product.create([title: "姚明的篮球",
                                  tag:"sportceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "大姚的那些高光时刻，作为一名受人尊重的球员及篮球运动的友好使者，这位２米２６的大个子改变了世界篮球运动的面貌，用中国人特有的坚韧和睿智，赢得了的肯定，让世界记住了他在赛场上的坚毅，在赛场外的亲善。他与篮球的故事，未完待续。"])
 puts "Product created"
 
 create_product = Product.create([title: "宁泽涛的耳机",
@@ -209,7 +221,8 @@ create_product = Product.create([title: "宁泽涛的耳机",
                                  tag:"sportceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "明明可以靠颜值，却偏要靠实力。游泳池里的宁泽涛帅气迷人，高挑有型的身材，携手汇集极限运动、音乐、街头文化和创新科技的美国潮流耳机品牌Skullcandy ，为所有激励人心的冒险者、创新者、开拓者带来世界级的音频产品和游戏产品，享受运动，享受音乐。"])
 puts "Product created"
 
 create_product = Product.create([title: "老徐手作",
@@ -222,7 +235,8 @@ create_product = Product.create([title: "老徐手作",
                                  tag:"movieceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "女神早已升级手艺人！从各种包包、陶艺到身上戴的各种饰品，甚至衣服，样样都是老徐手作！在过去一年多，她自己一针一线做了170多个包包，几百件小饰品。什么“人生赢家”，都是人生过客，只有心儿里美风雨无阻，年华无欺，谁也拿不走。每个人都把自己管好了，也许就是对社会最大的贡献。—徐裁缝"])
 puts "Product created"
 
 create_product = Product.create([title: "卡戴珊的礼服",
@@ -235,7 +249,8 @@ create_product = Product.create([title: "卡戴珊的礼服",
                                  tag:"netceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "时尚圈屹立不倒的超强基因“卡戴珊家族”，卡戴珊绝对是一个成功的女人，她的成功可能来自于名气，不过她的成功却不仅仅只有名气，2012年，卡戴珊作为一位时装设计师，建立了Kardashian Kollection时装品牌，简称KK，产品包括女包、连衣裙、香水、床上用品等等。每年收入数亿美金，堪比任何一个好莱坞巨星。你们看到的是八卦，姐活的是黄金人生。"])
 puts "Product created"
 
 create_product = Product.create([title: "TFboys的自行车",
@@ -248,7 +263,8 @@ create_product = Product.create([title: "TFboys的自行车",
                                  tag:"netceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "TFBOYS成员与日韩男子团体比起来更接地气，像邻家小男孩，略显稚气的唱功依然会被定义为“小朋友”角色。最大特点就是新鲜年轻，他们已经不再只歌唱“太阳花”和红领巾，他们唱着00后的感情观，传递着健康向上正能量，还有些情窦初开式的青涩告白。"])
 puts "Product created"
 
 create_product = Product.create([title: "鹿晗的配饰",
@@ -261,5 +277,6 @@ create_product = Product.create([title: "鹿晗的配饰",
                                  tag:"netceleb",
                                  aasm_state: "before_auction",
                                  begintime: "2016-11-15 18:00:00",
-                                 endtime: "2016-12-25 09:00:00"])
+                                 endtime: "2016-12-25 09:00:00",
+                                 product_story: "“小鲜肉”鹿晗，他的一举一动牵动着2571万粉丝的心情，不仅颜值有担当，而且对于配饰有极大的品位追求。眼镜、鞋子、配饰各方面搭配起来都是得心应手，总之长了一张帅Cry的脸，真是穿配什么都好看呢。颜值爆表萌萌哒，配饰选择有一套！"])
 puts "Product created"
